e46df40fb4f25472b1dcf124e01c6f145efffec3
arockburn <arockburn@gmail.com>
2015-02-25 20:35:59 -0500

    Program now works correctly for 6 days. Adapting to 5 days should be simple enough, and should be the purpose of the next commit.

--- src/RoutingAlgos.java
+++ src/RoutingAlgos.java
@@ -8,17 +8,17 @@ public class RoutingAlgos {
     public int[] stopCounts = new int[RoutingConstants.days];
 
     public int[] daysChoices(int freq){
-        int[] choices = new int[freq];
+        int[] choices = new int[RoutingConstants.days];
 
         if(freq >= RoutingConstants.days)
             Arrays.fill(choices, 1);
 
+        /*
         else{
             int gaps = RoutingConstants.days - freq;
             if(gaps > 1){
                 int daysLeft = freq;
                 Random rand = new Random();
-                boolean prevDayScheduled = false;
 
                 if(gaps >= freq) {
                     if(gaps == freq){
@@ -48,8 +48,37 @@ public class RoutingAlgos {
                                 choices[i] = 1;
                         }
                     }
-                    else{
+                    else if(gaps > freq){
+                        boolean prevDayScheduled = false;
                         int extraGaps = gaps - freq;
+                        for(int i = 0; i < RoutingConstants.days; i++){
+                            if(prevDayScheduled) {
+                                choices[i] = 0;
+                                prevDayScheduled = false;
+                                gaps--;
+                            }
+                            else{
+                                if(extraGaps > 0){
+                                    int r = rand.nextInt();
+                                    if(r == 1 && daysLeft != 0){
+                                        choices[i] = 1;
+                                        daysLeft--;
+                                        prevDayScheduled = true;
+                                    }
+                                    else{
+                                        choices[i] = 0;
+                                        extraGaps--;
+                                        gaps--;
+                                        prevDayScheduled = false;
+                                    }
+                                }
+                                else{
+
+                                }
+                            }
+                        }
+                    }
+                    else{
 
                         for(int i = 0; i < RoutingConstants.days; i++){
                             double useGap = rand.nextDouble() * (gaps/RoutingConstants.days);
@@ -83,6 +112,85 @@ public class RoutingAlgos {
             }
 
         }
+        */
+
+        else {
+            int period = RoutingConstants.days / freq;
+            int extraDays = RoutingConstants.days % freq;
+            int daysScheduled = 0;
+            boolean doneScheduling = false;
+            Random random = new Random();
+
+            while (!doneScheduling) {
+                if (daysScheduled == RoutingConstants.days)
+                    doneScheduling = true;
+
+                else if (period == 1) {
+                    if (extraDays >= 1) {
+                        int[] gapDays = new int[extraDays];
+                        for (int i = 0; i < extraDays; i++) {
+                            int rand = random.nextInt(RoutingConstants.days);
+                            if (i == 0)
+                                gapDays[0] = rand;
+                            else {
+                                if (gapDays[0] == rand)
+                                    gapDays[1] = (rand + 2) % (RoutingConstants.days - 1);
+                                else
+                                    gapDays[1] = 0;
+                            }
+                        }
+
+                        if (extraDays == 2) {
+                            if (gapDays[0] > gapDays[1]) {
+                                int temp = gapDays[0];
+                                gapDays[0] = gapDays[1];
+                                gapDays[1] = temp;
+                            }
+                        }
+
+                        int gapDayCnt = 0;
+                        boolean gapsUsed = false;
+                        for (int i = 0; i < RoutingConstants.days; i++) {
+                            boolean gap = false;
+                            if(!gapsUsed){
+                                if (i == gapDays[gapDayCnt]) {
+                                    choices[i] = 0;
+                                    gapDayCnt++;
+                                    gap = true;
+                                    if(gapDayCnt == extraDays)
+                                        gapsUsed = true;
+                                }
+                            }
+                            if(!gap)
+                                choices[i] = 1;
+
+                            daysScheduled++;
+                        }
+                    }
+
+                }
+                else{
+                    int dayToSchedule = random.nextInt(period);
+                    for (int i = 0; i < period; i++) {
+                        if (daysScheduled > 0) {
+                            if (choices[daysScheduled - 1] == 1 && dayToSchedule == i)
+                                dayToSchedule++;
+                        }
+                        if (i != dayToSchedule)
+                            choices[daysScheduled] = 0;
+                        else
+                            choices[daysScheduled] = 1;
+
+                        daysScheduled++;
+                    }
+                    if (extraDays != 0) {
+                        choices[daysScheduled] = 0;
+                        daysScheduled++;
+                        extraDays--;
+                    }
+                }
+            }
+        }
 
 
         return choices;
@@ -93,27 +201,27 @@ public class RoutingAlgos {
         int currStopFreq;
         int numStops;
         switch(day){
-            case 1:
+            case 0:
                 currStopFreq = 1;
                 numStops = RoutingConstants.oneDayStops;
                 break;
-            case 2:
+            case 1:
                 currStopFreq = 2;
                 numStops = RoutingConstants.twoDayStops;
                 break;
-            case 3:
+            case 2:
                 currStopFreq = 3;
                 numStops = RoutingConstants.threeDayStops;
                 break;
-            case 4:
+            case 3:
                 currStopFreq = 4;
                 numStops = RoutingConstants.fourDayStops;
                 break;
-            case 5:
+            case 4:
                 currStopFreq = 5;
                 numStops = RoutingConstants.fiveDayStops;
                 break;
-            case 6:
+            case 5:
                 currStopFreq = 6;
                 numStops = RoutingConstants.sixDayStops;
                 break;
@@ -136,10 +244,11 @@ public class RoutingAlgos {
     public static void main(String[] args){
         int[][] finalRoutes = new int[RoutingConstants.days][];
         RoutingAlgos algos = new RoutingAlgos();
-        for(int i = RoutingConstants.days; i > 0; i--){
-//            finalRoutes[i] = algos.routes(i);
-            System.out.println(i + ",");
+        for(int i = RoutingConstants.days - 1; i >= 0; i--){
+            finalRoutes[i] = algos.routes(i);
+//            System.out.println(i + ",");
         }
 
+        System.out.print(Arrays.toString(algos.stopCounts));
     }
 }
